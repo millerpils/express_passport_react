5f148a8f83845d768c966d347148d4ea8bdc9629
Miller <sbrn290@pfslap859648.enterprise.internal.city.ac.uk>
2019-03-28 10:29:19 +0000

    Use heroku URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Profile from './Components/Profile'
 import { BrowserRouter as Router} from "react-router-dom"
+const herokuURL = "https://cryptic-badlands-83200.herokuapp.com/"
+//const localURL = "http://localhost:8080/get-user/"
 
 class App extends React.Component {  
 
@@ -15,8 +17,7 @@ class App extends React.Component {
   }
 
   getUserJSON() {
-    //fetch("https://polar-springs-69108.herokuapp.com/get-user")
-    fetch("http://localhost:8080/get-user")
+    fetch(herokuURL)
       .then(response => {
         if (response.ok) {
           return response.json()
